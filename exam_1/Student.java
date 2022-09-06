34d6c81f1f5e51cd4a67988afbbe9a8f296cde52
respeciaev <111834370+respeciaev@users.noreply.github.com>
2022-09-06 09:45:07 +0800

    Add files via upload

---
+++ exam_1/Student.java
@@ -0,0 +1,37 @@
+public class Student {
+    //Character attributes
+    String surname;
+    String firstname;
+    char middleInitial;
+    String dateOfBirth;
+    String studentNumber;
+    String emailAddress;
+    Boolean IamAwesome;
+
+    //Character Method
+    public void sayMysurname() {
+        System.out.println("My surname is " + surname );
+    }
+    public void sayMyfirstname() {
+        System.out.println("My firstname is " + firstname );
+    }
+    public void middleInitial() {
+        System.out.println("My middleInitial is " + middleInitial );
+    }
+    public void sayMydateOfBirth() {
+        System.out.println("My Birthday is " + dateOfBirth );
+    }
+    public void sayMystudentNumber() {
+        System.out.println("My studentNumber is " + studentNumber );
+    }
+    public void sayMystudentemailAddress() {
+        System.out.println("My emailAddress is " + emailAddress );
+    }
+    public void sayMystudentIamAwesome() {
+        System.out.println("I Am Awesome " + IamAwesome );
+    }
+
+
+
+    
+}
