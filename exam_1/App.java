cf60355084b20a38ce64a3613c61fdf55c48b760
respeciaev <111834370+respeciaev@users.noreply.github.com>
2022-09-06 09:44:26 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) {
+
+    //Character Class
+    Student Von = new Student();
+
+    Von.surname = "Respecia";
+    Von.firstname = "Von";
+    Von.middleInitial = 'C';
+    Von.dateOfBirth = "12/10/2002";
+    Von.studentNumber = "2022109667";
+    Von.emailAddress = "respeciaea@students.national-u.edu.ph";
+    Von.IamAwesome = true;
+    Von.sayMystudentNumber();
+    Von.sayMystudentemailAddress();
+    Von.sayMystudentIamAwesome();
+
+
+
+
+    
+    }
+}
